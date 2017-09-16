834ded3bf1b3d49d7e8643cc4576612add7c263c
Juan Pablo Jimenez <jchantej@gmail.com>
2017-09-16 23:09:14 +0200

    Creación de la clase C21

---
+++ src/main/java/juan/jimenez/SVC/miw/upm/es/C21.java
@@ -0,0 +1,13 @@
+package juan.jimenez.SVC.miw.upm.es;
+
+public class C21 {
+    
+    public String m1() {
+        return "C21.m1";
+    }
+    
+    public String m2() {
+        return "C21.m2";
+    }
+
+}
