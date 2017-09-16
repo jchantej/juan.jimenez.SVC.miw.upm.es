975799b67a549ddf7b649c57113a5645b50480c8
Juan Pablo Jimenez <jchantej@gmail.com>
2017-09-16 23:00:58 +0200

    Creación de la clase C11

---
+++ src/main/java/juan/jimenez/SVC/miw/upm/es/C11.java
@@ -0,0 +1,13 @@
+package juan.jimenez.SVC.miw.upm.es;
+
+public class C11 {
+
+    public String m1() {
+        return "C11.m1";
+    }
+
+    public String m2() {
+        return "C11.m2";
+    }
+
+}
