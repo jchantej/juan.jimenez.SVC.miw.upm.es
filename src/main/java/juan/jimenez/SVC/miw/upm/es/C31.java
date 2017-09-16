4cc91a23f527cb7f87bd2cc992b9634829fe9531
Juan Pablo Jimenez <jchantej@gmail.com>
2017-09-17 00:07:47 +0200

    Creacion de la clase C31

---
+++ src/main/java/juan/jimenez/SVC/miw/upm/es/C31.java
@@ -0,0 +1,12 @@
+package juan.jimenez.SVC.miw.upm.es;
+
+public class C31 {
+    public String m1() {
+        return "C31.m1";
+    }
+    
+    public String m2() {
+        return "C31.m2";
+    }
+
+}
